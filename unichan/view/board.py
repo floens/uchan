2848fb5ff411646d2026b9666b72dcdffc3f7911
Floens <floens@gmail.com>
2016-02-16 20:01:02 +0100

    Improve board page switcher

--- unichan/view/board.py
+++ unichan/view/board.py
@@ -1,4 +1,4 @@
-from flask import render_template, abort
+from flask import render_template, abort, redirect, url_for
 
 from unichan import app
 from unichan import g
@@ -6,16 +6,25 @@ from unichan import g
 
 @app.route('/<board_name>/')
 @app.route('/<board_name>/<int:page>')
-def board(board_name, page=0):
-    board_cached = g.posts_cache.find_board_cached(board_name, page)
-    if not board_cached:
-        abort(404)
-
+def board(board_name, page=None):
     board_config_cached = g.board_cache.find_board_config_cached(board_name)
     if not board_config_cached:
         raise Exception('Board config cache None while board cache not')
 
-    if page < 0 or page >= board_config_cached.board_config.pages:
+    if page == 1:
+        return redirect(url_for('board', board_name=board_name))
+
+    if page is None:
+        page = 1
+
+    if page <= 0 or page > board_config_cached.board_config.pages:
         abort(404)
 
-    return render_template('board.html', board=board_cached.board, threads=board_cached.threads, board_config=board_config_cached.board_config)
+    page -= 1
+
+    board_cached = g.posts_cache.find_board_cached(board_name, page)
+    if not board_cached:
+        abort(404)
+
+    return render_template('board.html', board=board_cached.board, threads=board_cached.threads,
+                           board_config=board_config_cached.board_config, page_index=page)
