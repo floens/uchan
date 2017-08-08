4951f022f0317c06629ac61b3da530eb0bdd4cc2
Floens <floens@gmail.com>
2017-08-08 17:13:30 +0200

    Only wait on db for selected functions.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,23 +3,29 @@ set -e
 
 # Script for the docker endpoint, waits for the db, and executes whatever command.
 
-# Wait for postgres to come online
-until PGPASSWORD=uchan psql -h db -U "uchan" -c '\l' >/dev/null; do
-  >&2 echo "Waiting for postgres to be ready"
-  sleep 1
-done
+db_wait() {
+    # Wait for postgres to come online
+    until PGPASSWORD=uchan psql -h db -U "uchan" -c '\l' >/dev/null; do
+        >&2 echo "Waiting for postgres to be ready"
+        sleep 1
+    done
+}
 
 case "$1" in
     "app")
+        db_wait
         uwsgi /etc/uwsgi/uwsgi.ini
         ;;
     "worker")
+        db_wait
         celery worker --uid uchan -c 8 -A uchan:celery --logfile=/opt/app/data/log/worker-%n.log --loglevel=INFO
         ;;
     "upgrade")
+        db_wait
         alembic upgrade head
         ;;
     "setup")
+        db_wait
         python3 setup.py
         ;;
     "shell")
